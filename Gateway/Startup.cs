7ee49c126c61890a28a7afb0d504b577f5d49989
chandanys <chandan.kumar@udmglobal.com>
2019-10-25 12:23:20 +0530

    Added Gateway Project

---
+++ Gateway/Startup.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Gateway
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        private AppSettings appSettings;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+            appSettings = appSettingsSection.Get<AppSettings>();
+
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    RequireExpirationTime = true,
+                    ClockSkew = TimeSpan.FromSeconds(1)
+                };
+            });
+
+            services.AddCors();
+
+            services.AddOcelot(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials()
+                .WithHeaders());
+
+            app.UseAuthentication();
+
+            app.Use(async (ctx, next) =>
+            {
+                if (ctx.User.Identity.IsAuthenticated
+                        || appSettings.WhitelistedUrls.Any(s => ctx.Request.Path.Value.Equals(s, StringComparison.InvariantCultureIgnoreCase)))
+                {
+                    await next.Invoke();
+                }
+                else
+                {
+                    ctx.Response.StatusCode = 401;
+                    ctx.Response.Headers.Clear();
+                    ctx.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                }
+            });
+
+            app.UseOcelot().Wait();
+        }
+    }
+}
